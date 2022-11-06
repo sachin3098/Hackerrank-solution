f7203bf14efb68d1da5e9d04b067b106febcc003
sachin3098 <patelsachin3098@gmail.com>
2022-11-06 11:50:44 +0530

    add minmax and simple array solutions

---
+++ warm up/minmax.py
@@ -0,0 +1,6 @@
+def miniMaxSum(arr):
+    # Write your code here
+    x=sum(arr)
+    minsum=x-max(arr)
+    maxsum=x-min(arr)
+    print(minsum,maxsum)
---
+++ warm up/simplearraysum.py
@@ -0,0 +1,6 @@
+def simpleArraySum(ar):
+    # Write your code here
+    sum1=0
+    for i in range(len(ar)):
+        sum1=sum1+ar[i]
+    return sum1
