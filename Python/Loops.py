f5afeb9a7fd137c8891f064531372bac35e046cc
sachin3098 <patelsachin3098@gmail.com>
2022-11-07 23:03:23 +0530

    add file

---
+++ Python/Loops.py
@@ -0,0 +1,8 @@
+Problem Link:
+
+https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i**2)
